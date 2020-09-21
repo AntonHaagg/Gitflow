fb672fd5ca1535b0312a71b50f9d6abaf6e6a5e3
anton <anton@cityweb.se>
2020-09-21 10:12:05 +0200

    removed data

--- 1/src/main/java/package/App.java
+++ 1/src/main/java/package/App.java
@@ -10,11 +10,5 @@ public class App
     {
 
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Release1" );
-        System.out.println( "Develop change" );
-        System.out.println( "Release1 change" );
-        System.out.println( "Hotfix change" );
-        System.out.println( "Faeture 2" );
     }
 }
